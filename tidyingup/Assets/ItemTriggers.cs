fc62c266e2f3fccb4c8ccf12c9295790ca213355
Alejandra <av1658@nyu.edu>
2019-01-26 18:52:30 -0500

    this is the fixed version, pls work off of tis commit

---
+++ tidyingup/Assets/ItemTriggers.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ItemTriggers : MonoBehaviour {
+
+    private GameObject spawner;
+    private Spawner spawnerScript;
+
+    private GameObject manager;
+    private GameManager gameManager;
+
+    public int itemMax;
+    private int itemsThrown;
+
+    // Use this for initialization
+    void Start()
+    {
+        spawner = GameObject.Find("Spawner");
+        spawnerScript = spawner.GetComponent<Spawner>();
+
+        manager = GameObject.Find("GameManager");
+        gameManager = manager.GetComponent<GameManager>();
+
+        itemMax = 5;
+        itemsThrown = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (spawnerScript.itemCount > itemMax)
+        {
+            gameManager.itemsThrownOut = itemsThrown;
+            SceneManager.LoadScene("End");
+        }
+    }
+
+}
---
+++ tidyingup/Assets/Spawner.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    //Array of Objects
+    public GameObject[] items;
+  
+    //Spawning
+    public float spawnInterval = 5;
+    private float currentSpawnTime = 0;
+
+    //Timer
+    public float bigCountdown = 10; // 120 seconds is 2 minutes
+    private float currentBigTime = 0;
+
+    //Spawn Ranges
+    private readonly float xMin = -22.0f;
+    private readonly float xMax = 22.0f;
+
+    private readonly float zMin = -22.0f;
+    private readonly float zMax = 22.0f;
+
+    //Spawn locations
+    private readonly float itemY = 5;
+    private float itemX;
+    private float itemZ;
+
+    //Item index
+    private int index;
+
+    //itemCount
+    public int itemCount = 0;
+
+    void Update()
+    {
+        currentSpawnTime += Time.deltaTime;
+        currentBigTime += Time.deltaTime;
+
+        if (currentSpawnTime >= spawnInterval)
+        {
+            Spawn();
+            currentSpawnTime = 0;
+        }
+
+        if (currentBigTime >= bigCountdown)
+        {
+            if (spawnInterval != 1) {
+                spawnInterval -= 1;
+            }
+            currentBigTime = 0;
+        }
+    }
+
+    void Spawn()
+    {
+        itemX = Random.Range(xMin, xMax);
+        itemZ = Random.Range(zMin, zMax);
+
+        Vector3 itemPos = new Vector3(itemX, itemY, itemZ);
+
+        index = Random.Range(0, items.Length);
+
+        Instantiate(items[index], itemPos, Quaternion.identity);
+
+        itemCount++;
+
+        //Debug.Log(itemCount);
+    }
+}

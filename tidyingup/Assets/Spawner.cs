8a59bdd6b1137780dcd4f5de8cd4ce16e8f0fc58
Alejandra <av1658@nyu.edu>
2019-01-25 23:30:31 -0500

    Scripts

---
+++ tidyingup/Assets/Spawner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    //Array of Objects
+    public GameObject[] items;
+  
+    // Use this for initialization
+    public float spawnInterval;
+    public float currentSpawnTime;
+
+    public float bigCountdown; // 120 seconds is 2 minutes
+    public float currentBigTime;
+
+    void Start()
+    {
+        spawnInterval = 5;
+        currentSpawnTime = 0;
+
+        bigCountdown = 10; // 120 seconds is 2 minutes
+        currentBigTime = 0;
+}
+
+    void Update()
+    {
+        currentSpawnTime += Time.deltaTime;
+        currentBigTime += Time.deltaTime;
+
+        if (currentSpawnTime >= spawnInterval)
+        {
+            Spawn();
+            currentSpawnTime = 0;
+        }
+
+        if (currentBigTime >= bigCountdown)
+        {
+            spawnInterval -= 1;
+            currentBigTime = 0;
+        }
+    }
+
+    void Spawn()
+    {
+        Vector3 enemyPos = new Vector3(0,0,0);
+        Instantiate(items[0], enemyPos, Quaternion.identity);
+    }
+}

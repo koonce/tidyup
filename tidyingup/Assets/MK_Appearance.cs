016fb90a88dd44de45b8936bb0c5e575c9b4f02f
koonce <erk347@nyu.edu>
2019-01-27 14:36:32 -0500

    mary kondor pop up and text

--- tidyingup/Assets/MK_Appearance.cs
+++ tidyingup/Assets/MK_Appearance.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MK_Appearance : MonoBehaviour {
 
-    public string[] speech = { "", "", "", "", "", "", "", "", "", "" };
+    string[] speech = new string[]
+        {
+        "Make sure you follow the Konmari method!",
+        "Did that item spark joy?",
+        "I still must introduce myself to the house!",
+        "Do you want this to be a part of your future?",
+        "This is the most clutter I’ve ever seen!",
+        "Make sure to hold on to the things that matter!",
+        "This is so fun!",
+        "I love messes!",
+        "You must get to know your home through tidying!",
+        "say thank you before you throw your clutter away!",
+        "Be gentle!! GENTLE!!!",
+        };
     public int[] usedNumbers;
 
-    TextMesh text;
+    TextMeshPro text;
 
     public Vector3 onScreen;
     public Vector3 offScreen;
@@ -20,16 +34,24 @@ public class MK_Appearance : MonoBehaviour {
     public float timer = 10f;
     public float startTime = 10f;
 
+    public float bigCountdown = 10;
+    private float currentBigTime = 0;
+    private float littleCountdown = 3;
+
 	// Use this for initialization
 	void Start () {
         transform.localPosition = offScreen;
         enter();
-    }
-	
-	// Update is called once per frame
-	void Update () {
 
-        text = GameObject.Find("quote").GetComponent<TextMesh>();
+        text.GetComponent<Renderer>().sortingLayerID =
+        this.transform.parent.GetComponent<Renderer>().sortingLayerID;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        text = GameObject.Find("quote").GetComponent<TextMeshPro>();
 
         if (on)
         {
@@ -39,7 +61,12 @@ public class MK_Appearance : MonoBehaviour {
 
             if (transform.localPosition == onScreen)
             {
-                exit();
+                currentBigTime += Time.deltaTime;
+
+                if (currentBigTime >= littleCountdown)
+                {
+                    exit();
+                }
             }
         }
 
@@ -50,33 +77,27 @@ public class MK_Appearance : MonoBehaviour {
 
             if (transform.localPosition == offScreen)
             {
-                enter();
+                currentBigTime += Time.deltaTime;
+
+                if (currentBigTime >= bigCountdown)
+                {
+                    enter();
+                }
             }
         }
     }
 
     void enter()
     {
-        int j = Random.Range(0, 10);
+        int j = 0;
+        j = Random.Range(0, 10);
 
-        /*
-        int num;
-        bool used = false;
-        for (int i = 0; i < 10; i++)
-        {
-            if (usedNumbers[i] == j)
-            {
-                used = true;
-            }
-        } 
-
-    */
-
-        text.text = speech[j];
+        text.SetText(speech[j]);
         
         Debug.Log("enter");
         speed = 0f;
         on = true;
+        currentBigTime = 0;
 
     }
 
@@ -85,6 +106,7 @@ public class MK_Appearance : MonoBehaviour {
         Debug.Log("exit");
         speed = 0f;
         on = false;
-        
+        currentBigTime = 0;
+
     }
 }

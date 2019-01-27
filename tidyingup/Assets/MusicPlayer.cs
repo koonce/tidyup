45a71ee6af78e753abc12f3618e2c22e1eb9ac9f
Alejandra <av1658@nyu.edu>
2019-01-27 13:12:42 -0500

    MusicPlayerScript

---
+++ tidyingup/Assets/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null; 
+	
+    //Doesn't allow two play twice/over each other
+	void Awake(){
+		if (instance != null){
+			Destroy (gameObject);
+		} else{
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+ 
